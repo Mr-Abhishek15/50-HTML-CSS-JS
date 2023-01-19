3e769f1e0465cc170a9f577b66b4d5533e6d7b4b
Abhishek <shek79000@gmail.com>
2023-01-19 19:20:49 +0530

    push

---
+++ Background Image Scroll Effect/index.js
@@ -0,0 +1,11 @@
+const bgImageE1= document.getElementById("bg-image")
+
+window.addEventListener("scroll",()=>{
+    updateImage()
+})
+
+function updateImage(){
+    bgImageE1.style.opacity =1 -window.pageYOffset/800
+    console.log(window.pageYOffset, 1- window.pageYOffset/ 900);
+    bgImageE1.style.backgroundSize = 160- window.pageYOffset / 12 +"%"
+}
