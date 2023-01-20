33759758707d8c1d650f438a79ecd8c88c54c500
Abhishek <shek79000@gmail.com>
2023-01-20 18:55:34 +0530

    Completed

--- Background Image Scroll Effect/index.js
+++ Background Image Scroll Effect/index.js
@@ -6,7 +6,8 @@ window.addEventListener("scroll",()=>{
 
 
 function updateImage(){
+    
     bgImageE1.style.opacity =1 -window.pageYOffset/800
     console.log(window.pageYOffset, 1- window.pageYOffset/ 900);
     bgImageE1.style.backgroundSize = 160- window.pageYOffset / 12 +"%"
-}
+} 
