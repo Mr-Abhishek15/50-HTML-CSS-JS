9a7d0d8fd535d46c041b48bca51875df0ff2e6b7
Abhishek <shek79000@gmail.com>
2023-02-12 14:42:02 +0530

    completed

---
+++ Real time character/index.js
@@ -0,0 +1,14 @@
+const textareaE1 = document.getElementById("textarea");
+const totalCounterE1 = document.getElementById("total-counter");
+const remainingCounterE1 = document.getElementById("remaining-counter");
+
+textareaE1.addEventListener("keyup", () => {
+    updateCounter();
+});
+
+updateCounter()
+
+function updateCounter(){
+    totalCounterE1.innerText = textareaE1.value.length;
+    remainingCounterE1.innerText = textareaE1.getAttribute("maxlength") - textareaE1.value.length;
+}
