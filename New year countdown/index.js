f8667e5ddf304cb842c49135b063cdede671178d
Abhishek <shek79000@gmail.com>
2023-03-06 10:09:21 +0530

    updated the js

--- New year countdown/index.js
+++ New year countdown/index.js
@@ -3,26 +3,24 @@ const hourE1 = document.getElementById("hour");
 const minuteE1 = document.getElementById("minute");
 const secondE1 = document.getElementById("second");
 
-const newYearTime = new Date("Jan 1, 2023 00:00:00").getTime()
- 
-updateCountdown()
+const newYearTime  = new Date("Jan 1, 2024 00:00:00").getTime();
 
-function updateCountdown(){
+updateCountdown();
+
+function updateCountdown() {
     const now = new Date().getTime();
-    const gap =newYearTime - now
-    
+    const gap = newYearTime - now;    
     const second = 1000;
-    const minute = second*60;
-    const hour = minute*60;
-    const day = hour*24;
-
-    const d = math.floor(gap/day);
-    const h = math.floor((gap % day) / hour);
-    const m = math.floor((gap % hour) / minute);
-    const s = math.floor((gap % minute ) / second);
-    dayE1.innerText= d;
-    hourE1.innerText= h;
-    minuteE1.innerText= m;
-    secondE1.innerText= s;
-    setTimeout(updateCountdown, 1000)
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const d = Math.floor(gap / day);
+    const h = Math.floor((gap % day) / hour);
+    const m = Math.floor((gap % hour) / minute);
+    const s = Math.floor((gap % minute) / second);
+    dayE1.innerText = d;
+    hourE1.innerText = h;
+    minuteE1.innerText = m;
+    secondE1.innerText = s; 
+    setTimeout(updateCountdown, 1000);
 }
